3ede6c72f53d2f70e5d888f88747a11945b2d87d
Jeremy Rempel <jeremy.rempel@gmail.com>
2019-06-29 15:25:05 -0400

    rename tests

--- app/src/test/java/com/github/jeremyrempel/myapplication/MainFragmentTest.kt
+++ app/src/test/java/com/github/jeremyrempel/myapplication/MainFragmentTest.kt
@@ -20,7 +20,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class JvmModelFragmentTest {
+class MainFragmentTest {
 
     @MockK
     private lateinit var fakeViewModel: MainFragmentViewModel
